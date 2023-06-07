f60ece82c4191070821de8e0747daa729e886b9d
asally <a.salawu@outlook.com>
2023-06-07 18:11:15 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args){
+            // this is a comment 
+            Console.WriteLine("hello world");
+        }
+    }
+}

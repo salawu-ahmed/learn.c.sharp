e0639a2714e7137779fab02dbe25d22625632700
asally <a.salawu@outlook.com>
2023-06-07 18:22:56 +0000

    types of variables & constants

--- Program.cs
+++ Program.cs
@@ -3,11 +3,28 @@ using System;
 namespace HelloWorld {
     class Program {
         static void Main(string[] args){
-            /* 
-                the code below will print the words "hello world"
-                to the screen 
-            */
-            Console.WriteLine("hello world");
+            // an integer variable 
+            int age = 35;
+            Console.WriteLine(age);
+
+            // this is a constant variable (read-only)
+            const int birthYear = 2003;
+
+            // a double variable
+            double amount = 55.60
+            Console.WriteLine(amount);
+
+            // a string variable 
+            string name = "john";
+            Console.WriteLine(name);
+
+            // a char variable 
+            char tag ='X';
+            Console.WriteLine(tag);
+
+            // a boolean variabla
+            bool isEnabled = true;
+            Console.WriteLine(isEnabled);
         }
     }
 }
